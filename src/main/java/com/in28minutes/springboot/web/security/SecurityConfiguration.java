a8bfdf0fd6c7ce805ed3f496958f35fd2829eca3
macos <s.debojyoti@yahoo.com>
2022-06-13 00:31:07 +0530

    Initial Commit

--- src/main/java/com/in28minutes/springboot/web/security/SecurityConfiguration.java
+++ src/main/java/com/in28minutes/springboot/web/security/SecurityConfiguration.java
@@ -1,6 +1,12 @@
 package com.in28minutes.springboot.web.security;
 
 // import org.springframework.beans.factory.annotation.Autowired;
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 // import org.springframework.context.annotation.Configuration;
 // import org.springframework.http.HttpMethod;
@@ -58,7 +64,15 @@ public class SecurityConfiguration {
                 });
 
 
-        return http.build();
+       // return http.build();
+
+
+        return http
+                .requiresChannel(channel ->
+                        channel.anyRequest().requiresSecure())
+                .authorizeRequests(authorize ->
+                        authorize.anyRequest().permitAll())
+                .build();
     }
 	
 //	Previous version that securing all endpoints with HTTP basic
@@ -71,5 +85,31 @@ public class SecurityConfiguration {
 //        http.csrf().disable();
 //        http.headers().frameOptions().disable();
 //    }
+
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                var securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                var collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+        tomcat.addAdditionalTomcatConnectors(getHttpConnector());
+        return tomcat;
+    }
+
+    private Connector getHttpConnector() {
+        var connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        connector.setScheme("http");
+        connector.setPort(8080);
+        connector.setSecure(false);
+        connector.setRedirectPort(8443);
+        return connector;
+    }
 
 }
